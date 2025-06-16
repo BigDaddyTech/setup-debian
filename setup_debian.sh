7c134354a1777c3d281dd945f5c898e4fc051024
BigDaddyTech <79559602+BigDaddyTech@users.noreply.github.com>
2025-06-15 22:26:08 -0500

    Add setup_debian.sh

---
+++ setup_debian.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install GNOME and other utilities
+apt install gnome-shell gnome-terminal curl fastfetch gnome-disk-utility mangohud tree gnome-calculator git
+
+# Cleanup bloat from gnome-shell
+apt remove malcontent yelp nm-connection-editor im-config
+
+# Setup aliases in .bashrc
+echo 'alias ll="ls -la"' >> ~/.bashrc
+
+# Install Brave
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# Install Steam
+dpkg --add-architecture i386
+echo 'deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+apt update
+apt install steam-libs steam
+
+# Install Discord
+discord_url="https://discord.com/api/download?platform=linux&format=deb"
+curl -L -o /tmp/discord.deb $discord_url
+apt install /tmp/discord.deb
+
+# Install VSCodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+apt update
+apt install codium
+
+# Install FreeCAD
+add-apt-repository ppa:freecad-maintainers/freecad-stable
+apt update
+apt install freecad
